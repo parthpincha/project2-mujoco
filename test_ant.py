bcb752cc6bc3286758063594152121fb8c541dff
parthpincha <parthpincha@gmail.com>
2026-05-31 02:48:22 +0530

    second commit

---
+++ test_ant.py
@@ -0,0 +1,14 @@
+import gymnasium as gym
+
+env = gym.make("Humanoid-v5", render_mode="human")
+
+obs, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        obs, info = env.reset()
+
+env.close()
